01c5429e3f0931ff907815610f7e1f2aad68c309
Daniel Habig <danielth101@gmail.com>
2015-11-07 23:10:45 -0500

    added sdl image static lib project added loader code for sdl images

--- source/engine/asset/loader.h
+++ source/engine/asset/loader.h
@@ -10,5 +10,8 @@ namespace engine
 
 		const util::JSON FileLoaderJson(const std::string& szFilePath);
 		const util::JSON DataLoaderJson(const TBuffer& data);
+
+		const SDL_Texture* const FileLoaderSdlTexture(const std::string& szFilePath);
+		const SDL_Texture* const DataLoaderSdlTexture(const TBuffer& data);
 	}
 }
