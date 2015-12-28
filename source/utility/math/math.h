fa1735768674a71824120ae2372c12f83a18afeb
Daniel <danielth101@gmail.com>
2015-12-28 15:37:12 -0500

    added decompose

--- source/utility/math/math.h
+++ source/utility/math/math.h
@@ -4,6 +4,7 @@
 #include "glm/glm.hpp"
 #include "glm/matrix.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
 
 typedef glm::f32vec2		vec2;
 typedef glm::f32vec3		vec3;
