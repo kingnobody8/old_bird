7bb60a88874c42947099887dd0f8b5dd33f11ced
Daniel Habig <danielth101@gmail.com>
2016-04-28 23:37:31 -0400

    adsf

---
+++ rube/test_scene.rube-backups/test_scene.rube-scriptLog-160428-231105.rs
@@ -0,0 +1,1 @@
+//Restarted script log at 04/28/16 23:11:05
--- rube/test_scene.rube-backups/test_scene.rube-scriptLog.rs
+++ rube/test_scene.rube-backups/test_scene.rube-scriptLog.rs
@@ -1,1 +1,10 @@
-//Restarted script log at 04/28/16 23:11:05
+//Started script log at 04/28/16 23:36:22
+
+getBody(13).select();
+getBody(13).deselect();
+getFixture(13).delete();
+getBody(13).delete();
+getBody(6).select();
+getBody(6).deselect();
+getFixture(6).delete();
+getBody(6).delete();
